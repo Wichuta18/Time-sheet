61c417265c33098ee36715c108a23d1441c683d5
Wichuta18 <kimweir9@gmail.com>
2019-11-20 12:56:26 +0700

    Update Project

---
+++ application/views/Body.php
@@ -0,0 +1,30 @@
+<body>
+
+<div id="colorlib-page">
+    <a href="#" class="js-colorlib-nav-toggle colorlib-nav-toggle"><i></i></a>
+    <aside id="colorlib-aside" role="complementary" class="js-fullheight text-center">
+        <h1 id="colorlib-logo"><a href="index.html">Time sheet<span>.</span></a></h1>
+        <nav id="colorlib-main-menu" role="navigation">
+            <ul>
+                <li class="colorlib-active"><a href="index.html">Proflie</a></li>
+                <li><a href="photography.html">Time Tracker</a></li>
+                <li><a href="travel.html">Dashbord</a></li>
+                <li><a href="fashion.html">Project</a></li>
+                <li><a href="about.html">Member</a></li>
+                
+            </ul>
+        </nav>
+
+        <div class="colorlib-footer">
+            <p><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+          Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="icon-heart" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
+            <ul>
+                <li><a href="#"><i class="icon-facebook"></i></a></li>
+                <li><a href="#"><i class="icon-twitter"></i></a></li>
+                <li><a href="#"><i class="icon-instagram"></i></a></li>
+                <li><a href="#"><i class="icon-linkedin"></i></a></li>
+            </ul>
+        </div>
+    </aside> <!-- END COLORLIB-ASIDE -->
+</div>
+</body>
