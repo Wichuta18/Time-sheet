270faafe92ed52fea60a119235f49569a4e7c3cf
Wichuta18 <kimweir9@gmail.com>
2019-11-22 14:14:23 +0700

    update project

---
+++ application/models/project_model.php
@@ -0,0 +1,29 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Employee_model extends CI_Model {
+
+	function __constuct(){
+		parent::__constuct();
+	}
+    public function index()
+	{
+		return $this->db->get('projects');
+		
+	}
+    public function select_prj($idProject)
+    {
+		$query = $this->db->get_where("project",array("idProject"=>$idProject));
+		
+		$data = $query->result(); 
+		
+		return $data;
+	}
+
+	// public function insert_emp($data) { 
+	// 	if ($this->db->insert("employees", $data)) { 
+	// 	   return true; 
+	// 	} 
+	// 	else Echo "error";
+    //  } 
+}
--- application/views/project.php
+++ application/views/project.php
@@ -15,6 +15,7 @@
           <div class="container">
             <!-- Trigger the modal with a button -->
             <button type="button" class="btn btn-info btn-lg" data-toggle="modal" data-target="#exampleModal" data-whatever="@getbootstrap">Create Project</button>
+
             <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
               <div class="modal-dialog" role="document">
                 <div class="modal-content">
@@ -24,23 +25,23 @@
                       <span aria-hidden="true">&times;</span>
                     </button>
                   </div>
-                  <div class="modal-body">
+                  <div  role="form" id="add_project" method="post">
                     <form>
                       <div class="form-group">
                         <label for="recipient-name" class="col-form-label">Enter project code:</label>
-                        <input type="text" class="form-control" id="recipient-name">
+                        <input type="text" class="form-control" id="proCode">
                       </div>
                       <div class="form-group">
                         <label for="recipient-name" class="col-form-label">Enter project name:</label>
-                        <input type="text" class="form-control" id="recipient-name">
+                        <input type="text" class="form-control" id="ProName">
                       </div>
                       <div class="form-group">
                         <label for="recipient-name" class="col-form-label">Budget expenses:</label>
-                        <input type="text" class="form-control" id="recipient-name">
+                        <input type="text" class="form-control" id="Budget">
                       </div>
                       <div class="form-group">
                         <label for="recipient-name" class="col-form-label">Total duration:</label>
-                        <input type="text" class="form-control" id="recipient-name">
+                        <input type="text" class="form-control" id="Total">
                         <div class="input-group-append">
                         </div>
                       </div>
@@ -57,7 +58,7 @@
                   </div>
                   <div class="modal-footer">
                     <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                    <button type="button" class="btn btn-info btn-lg">Save Project</button>
+                    <button type="button" id="savePro" class="btn btn-info btn-lg">Save Project</button>
                   </div>
                 </div>
               </div>
@@ -98,3 +99,31 @@
             </table>
             <!-- <p class="mb-4">I am A Blogger Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean.</p> -->
             <!-- <p><a href="#" class="btn-custom">More About Me <span class="ion-ios-arrow-forward"></span></a></p> -->
+            
+            <script>
+              $(document).ready(function() {
+                $('#savePro').click(function() {
+                  var proCode = $('#proCode').val();
+                  var ProName = $('#ProName').val();
+                  var Budget = $('#Budget').val();
+                  var Total = $('#Total').val();
+                  alert(lname);
+                  $.ajax({
+                    method: "POST",
+                    url: "insert.php",
+                    data: $('#add_project').serialize(),
+                    beforeSend: function() {
+                      $('#response').html('<span class="text-info">Loading response...</span>');
+                    },
+                    success: function(data) {
+                      $('form').trigger("reset");
+                      $('#response').fadeIn().html(data);
+                      setTimeout(function() {
+                        $('#response').fadeOut("slow");
+                      }, 5000);
+                    }
+                  });
+
+                });
+              });
+            </script>
