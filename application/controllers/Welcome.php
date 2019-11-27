72ae1494930b324113cbe5e1bed279172b322cd0
unknown <slotsnow0301@gmail.com>
2019-11-27 12:46:36 +0700

    commit con / model

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -1,69 +1,83 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
-class Welcome extends CI_Controller {
+class Welcome extends CI_Controller
+{
 
-	/**
-	 * Index Page for this controller.
-	 *
-	 * Maps to the following URL
-	 * 		http://example.com/index.php/welcome
-	 *	- or -
-	 * 		http://example.com/index.php/welcome/index
-	 *	- or -
-	 * Since this controller is set as the default controller in
-	 * config/routes.php, it's displayed at http://example.com/
-	 *
-	 * So any other public methods not prefixed with an underscore will
-	 * map to /index.php/welcome/<method_name>
-	 * @see https://codeigniter.com/user_guide/general/urls.html
-	 */
-	public function index()
-	{
-		$this->load->view('header');
-		$this->load->view('body');
-		$this->load->view('js');
-	}
-	public function project()
-	{
+    /**
+     * Index Page for this controller.
+     *
+     * Maps to the following URL
+     * 		http://example.com/index.php/welcome
+     *	- or -
+     * 		http://example.com/index.php/welcome/index
+     *	- or -
+     * Since this controller is set as the default controller in
+     * config/routes.php, it's displayed at http://example.com/
+     *
+     * So any other public methods not prefixed with an underscore will
+     * map to /index.php/welcome/<method_name>
+     * @see https://codeigniter.com/user_guide/general/urls.html
+     */
+    public function index()
+    {
+        $this->load->view('header');
+        $this->load->view('body');
+        $this->load->view('js');
+    }
+    public function project()
+    {
+        $this->load->view('header');
+        $this->load->view('body');
+
         $this->load->model("Test_model");
-        $data["select_data"]= $this->Test_model->select_data();
-		$this->load->view('header');
-		$this->load->view('body');
-        $this->load->view('Test_view', $data);
-		$this->load->view('js');
-	}
-	public function form_validation(){
-        // echo 'hello world';
-        $this->load->library('form_validation');
-        $this->form_validation->set_rules("projectCode","projectCode",
-        'required');
-        $this->form_validation->set_rules("projectName","projectName",
-        'required|alpha');
-        $this->form_validation->set_rules("budget","budget",
-        'required');
-        $this->form_validation->set_rules("total","total",
-        'required');
+        $this->load->library("pagination");
+        $config['base_url'] = base_url() . 'project';
+        $config['uri_segement'] = 3;
+        $config['per_page'] = 5;
+        $config['first_tag_open'] = '<li>';
+        $config['first_tag_close'] = '</li>';
+        $config['last_tag_open'] = '<li>';
+        $config['last_tag_close'] = '</li>';
+        $config['prev_tag_open'] = '<li>';
+        $config['prev_tag_close'] = '</li>';
+        $config['next_tag_open'] = '<li>';
+        $config['next_tag_close'] = '</li>';
+        $config['num_tag_open'] = '<li>';
+        $config['num_tag_close'] = '</li>';
+        $config['cur_tag_open'] = '<li class="active"><a href="#">';
+        $config['cur_tag_close'] = '</a></li>';
+        $config['next_link'] = '&gt;';       
+        $config['prev_link'] = '&gt;';    
+        $config['num_link'] = 1;
+        
 
-        if($this->form_validation->run()){
-            //true
+        $page = $this->uri->segment(3,0);
+        $data["select_data"] = $this->Test_model->select_data($config['per_page'], $page);
+        $config['total_rows'] = $this->Test_model->getTotalrows();
+        $this->pagination->initialize($config);
+        $data['pagination'] = $this->pagination->create_links();
+
+        $this->load->view('Test_view', $data);
+        $this->load->view('js');
+    }
+    public function insert_project()
+    {
+        // echo 'hello world';
             $this->load->model("Test_model");
             $data = array(
-                "projectCode" =>$this->input->post("projectCode"),
-                "projectName" =>$this->input->post("projectName"),
-                "budget" =>$this->input->post("budget"),
-                "total_dur" =>$this->input->post("total")
+                "projectCode" => $this->input->post("projectCode"),
+                "projectName" => $this->input->post("projectName"),
+                "budget" => $this->input->post("budget"),
+                "startDate" => $this->input->post("startD"),
+                "endDate" => $this->input->post("endD")
             );
             $this->Test_model->insert($data);
 
             redirect(base_url() . "inserted");
-
-        }
-        else{
-            redirect(base_url() . "form_validation");
-        }
-    }
-    public function inserted(){
+        } 
+    public function inserted()
+    {
         $this->project();
     }
 }
--- application/models/Test_model.php
+++ application/models/Test_model.php
@@ -3,6 +3,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Test_model extends CI_Model {
 
+	private $lastQuery = '';
+
 	function __constuct(){
 		parent::__constuct();
 		$this->load->database();
@@ -10,8 +12,17 @@ class Test_model extends CI_Model {
     function insert($data){
         $this->db->insert("projects",$data);
 	}
-	function select_data(){
+	function select_data($limit, $start){
+		$this->db->limit($limit, $start);
 		$query = $this->db->get("projects");
+		$this->lastQuery = $this->db->last_query();
 		return $query;
+	}
+
+	function getTotalrows(){
+		$sql = explode('LIMIT',$this->lastQuery);
+		$query = $this->db->query($sql[0]);
+		$result = $query->result();
+		return count($result);
 	}
 } 
